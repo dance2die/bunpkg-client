b0d73f0c5fe795b69e9835d476ca2492c66c7c31
Sung Kim <sungmkim80@gmail.com>
2018-09-11 17:54:20 -0400

    Added sub header

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-const AppHeader = () => <header className="app-header">🍔 Bunpkg 🍔</header>;
+const AppHeader = () => (
+  <header className="app-header">
+    <h1>🍔 Bunpkg 🍔</h1>
+    <span className="app-subheader">{"B{uild}unpkg{URL}"}</span>
+  </header>
+);
 
 export default AppHeader;
