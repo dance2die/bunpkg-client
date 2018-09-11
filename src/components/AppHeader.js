40184ffba5a74aba62e14cca27177dbf8776e536
Sung Kim <sungmkim80@gmail.com>
2018-09-11 17:45:40 -0400

    Made steps font size responsive

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-const AppHeader = () => <header>🍔 Bunpkg 🍔</header>;
+const AppHeader = () => <header className="app-header">🍔 Bunpkg 🍔</header>;
 
 export default AppHeader;
