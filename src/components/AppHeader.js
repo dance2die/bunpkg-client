883432edab080952c0dded59fcd636a9eca3ea1a
Sung Kim <sungmkim80@gmail.com>
2018-09-11 21:37:19 -0400

    Updated subtitle color and added link to title

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -4,7 +4,9 @@ import Emoji from "./Emoji";
 const AppHeader = () => (
   <header className="app-header">
     <h1>
-      <Emoji label="Bun">🍔</Emoji> Bunpkg <Emoji label="Bun">🍔</Emoji>
+      <a href="https://www.bunpkg.com">
+        <Emoji label="Bun">🍔</Emoji> Bunpkg <Emoji label="Bun">🍔</Emoji>
+      </a>
     </h1>
     <span className="app-subheader">{"B{uild}unpkg{URL}"}</span>
   </header>
