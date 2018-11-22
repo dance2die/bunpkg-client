33d3458f88c8161c30ebc9920ca459e7bd59bea8
Sung Kim <sungmkim80@gmail.com>
2018-11-22 17:40:57 -0500

    CSS changes & Credit removal

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -2,38 +2,12 @@ import React, { Fragment } from "react";
 import Emoji from "./Emoji";
 import { ExternalLink } from "./Links";
 
-const Credit = () => (
-  <Fragment>
-    Many thanks <Emoji label="Thanks">🙏</Emoji> to
-    <div>
-      <ExternalLink href="https://twitter.com/mjackson">
-        Michael Jackson
-      </ExternalLink>(<ExternalLink href="https://unpkg.com/">
-        Unpkg
-      </ExternalLink>{" "}
-      &{" "}
-      <ExternalLink href="https://reactpodcast.simplecast.fm/19">
-        React Podcast Ep 19
-      </ExternalLink>)
-    </div>
-    <div>
-      <ExternalLink href="https://github.com/pastelsky">
-        Shubham Kanodia
-      </ExternalLink>
-      (<ExternalLink href="https://bundlephobia.com/">
-        BundlePhobia
-      </ExternalLink>)
-    </div>
-    <div>as I learned from their open source projects</div>
-  </Fragment>
-);
-
 const SourceCode = () => (
   <Fragment>
     <ExternalLink href="https://github.com/dance2die/bunpkg-client">
       bunpkg-client
-    </ExternalLink>
-    &amp;
+    </ExternalLink>{" "}
+    &amp;{" "}
     <ExternalLink href="https://github.com/dance2die/bunpkg-server">
       bunpkg-server
     </ExternalLink>
@@ -49,11 +23,6 @@ const AppFooter = () => (
       </Emoji>{" "}
       by <ExternalLink href="https://sungkim.co/">Sung Kim</ExternalLink>
     </div>
-    &mdash;
-    <div className="credit">
-      <Credit />
-    </div>
-    &mdash;
     <div className="source-code">
       <SourceCode />
     </div>
