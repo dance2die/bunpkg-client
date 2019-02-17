3c483af150fdd07dd0fdec5f350ce8b37e72fe9c
Sung M. Kim <sungmkim80@gmail.com>
2019-02-17 10:31:43 -0500

    Fixed react-app-rewired configuration
    Deployment failed due to change in react-app-rewired - https://github.com/timarney/react-app-rewired/issues/348

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,27 @@
-const { injectBabelPlugin } = require("react-app-rewired");
+// https://github.com/timarney/react-app-rewired/issues/348#issuecomment-452199363
+const { override, fixBabelImports, addLessLoader } = require("customize-cra");
 
-/**
- * {@link https://ant.design/docs/react/use-with-create-react-app#Use-babel-plugin-import|AntD-Use babel-plugin-import}
- */
-module.exports = function override(config, env) {
-  config = injectBabelPlugin(
-    ["import", { libraryName: "antd", libraryDirectory: "es", style: "css" }],
-    config
-  );
-  return config;
-};
+module.exports = override(
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: true // change importing css to less
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { "@primary-color": "#1DA57A" }
+  })
+);
+
+// const { injectBabelPlugin } = require("react-app-rewired");
+
+// /**
+//  * {@link https://ant.design/docs/react/use-with-create-react-app#Use-babel-plugin-import|AntD-Use babel-plugin-import}
+//  */
+// module.exports = function override(config, env) {
+//   config = injectBabelPlugin(
+//     ["import", { libraryName: "antd", libraryDirectory: "es", style: "css" }],
+//     config
+//   );
+//   return config;
+// };
